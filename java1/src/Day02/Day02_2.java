178d835ee26ccddfc33576088ef7fd6677f01b42
504 <504@DESKTOP-MURRJDF>
2022-03-02 17:53:15 +0900

    day02

---
+++ java1/src/Day02/Day02_2.java
@@ -0,0 +1,65 @@
+package Day02;
+
+public class Day02_2 	
+{ public static void main(String[] args) {
+	
+	byte var1 = -128; System.out.println(var1);
+	byte var2 = -30; System.out.println(var2);
+	byte var3 = 30; System.out.println(var3);
+	//byte var4 = 300; System.out.println(var4);
+		//오류발생 ; 128 이상은 저장할 수 없다.
+	byte var4 = 'j'; System.out.println(var4);
+	// 문자 -> 정수
+	// p.43 char : 2바이트[1문자] 유니코드(자바:전세계 모든 문자) vs 아스키코드
+	char c1 = 'a'; System.out.println( c1 );
+	char c2 = 97; System.out.println( c2 );		//10진수 표현
+		//정수 -> 문자 변환
+	char c3 ='\u0041'; System.out.println( c3 );	//16진수 표현 :	\u
+	char c4 = '가'; System.out.println( c4 );
+	char c5 = 44032; System.out.println( c5 );
+	char c6 = '\uac00'; System.out.println( c6 );
+	// 진법 [왜 ??? : 컴퓨터가 2진수만 사용하면 표현단위 적다---> 표현단위 다양성
+		//2진수 : 0 ,1
+		//8진수 : 0 ~ 7
+		//10진수 : 0 ~ 9
+		//16진수 : 0 ~ 9 a b c d e f
+	
+	short num1 = 30000; System.out.println( num2 );
+	
+	int num2 = 10; System.out.println( num2 ); //10진수
+	int num3 = 012; System.out.println( num3 );// 8진수는 앞에 0 붙이기
+	int num4 =0xA;	System.out.println(	num4 ); //16진수는 앞에 0x 붙이기
+	
+	long num5 = 10000000000;	System.out.println( num5);		
+	[데이터 뒤에 L] INT형 -> long형 
+	//
+	
+	//p.47~48 float[ 데이터 뒤에 f ] : 4바이트 / double (실수 기본타입): 8바이트 ;17자리
+	//뭐가 더 크다만 알면 됨
+	double num6 = 3.14; System.out.println( num6 );
+	float num7 = 3.14f; System.out.println( num7 );
+	
+	double num8 = 
+	float num9 =123456789
+	
+	//p.48 boolean : 1비트 [true 혹은 false]
+		//스위치 : on, off
+	boolean bo1 = true; System.out.println( bo1 );
+	
+		//p.49 : 타입변환
+			//*크기순서 : byte -> short -> int-> long -> float -> double
+			//1.자동 형변환
+				//작은 상자에서 큰상자로 이동가능
+				//큰상자에서 작은 상자로 이동 불가
+			byte 바이트 = 10;
+			int 인트 = 바이트;//자동 형변환 o
+			short 쇼트= 인트;//자동 형변환 x 오류 발생
+	
+			//2. 강제 형변환 ( 캐스트 )
+				//큰 상자에서 작은 상자로 이동 가능인데... 단 : 데이터 손실
+				//(변경할자료형)변수명
+			short 쇼트 = short(인트)// int형 변수를 short로 변환하는데 데이터 잘림
+					
+}
+
+}
---
+++ java1/src/Day02/Day02_4.java
@@ -0,0 +1,67 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class Day02_4 {
+	
+	public static void main(String[] args) {
+		//1.입력->저장>출력//세고 빼고
+		Scanner scanner = new Scanner(System.in);
+		//2.입력받을 데이터 -> 변수로 이동		
+		System.out.println("금액 : ");	int 금액 = scanner.nextInt();
+		//3.연산
+		System.out.println(" 입력한 금액 : " + 금액);
+		System.out.println(" 십만원 : " + (금액/100000) + "장");
+		
+		//금액 = 금액 -( 금액/100000)*100000;
+		금액 -= 금액 -( 금액/10000)*10000; //금액에서 십만원 단위 제외
+		System.out.println(" 만원 :"+ (금액/10000) + "장");
+
+		System.out.println(" 문제3) 정수 입력:");	 int 문제3 = scanner.nextInt();// 하나의 정수를 입력
+		System.out.println(" 입력한 정수는 7의 배수 판단 :" +( 문제3%7 == 0));
+	
+		
+	    
+	    //금액 = 금액 -( 금액/100000)*100000;
+	   
+	    // %f : 실수 표현
+	    	// %.숫자f		// %2.f :소수 2자리 까지 표현
+	    					// %5.f :소수 5자리 까지 표현
+	   //문제10 : 키를 정수를 입력받아 표준체중 출력하기
+	    		// 표준체중 계산식 =>(키 - 100)*0.9
+		System.out.println("문제10) 키: "); int 키 = scanner.nextInt();
+		System.out.println("표준체중: " + (키-100)*0.9);
+		
+		//문제11 키와 몸무게를 입력받아 BMI 출력하기
+				//BMI 계산식 => 몸무게/((키/100)*(키/100))
+		System.out.println("문제11) 키 :"); int 키 = scanner.nextInt();
+		System.out.println("문제11) 몸무게 : "); int 몸무게 =scanner.nextInt();
+		System.out.println("BMI: " + 몸무게/((키/100)*(키/100)));
+		//double 로하는게 낫다 키/100.0로 계산 마지막 printf
+		
+		//문제12 inch를 입력받아 cm로 고치기
+		//* 1inch=2.54cm
+		System.out.println("문제12) inch"); int inch =scanner.nextInt();
+		System.out.println("inch -> cm : "+ 2.54*inch);
+		
+		//문제13 
+		//중간고사 , 기말고사 ,수행평가를 입력 받아 반영비율 계산하기
+			//소수 둘째 자리까지 점수 출력하기
+		//중간고사 반영비율 =>30% //기말고사 반영비율 =>30%  //수행평가 반영비율 => 40%
+		
+		System.out.print(" 문제13) 중간고사 : "); double 중간고사 = scanner.nextdouble();
+		System.out.print(" 문제13) 기말고사 : "); double 기말고사 = scanner.nextDouble();
+		System.out.print(" 문제13) 수행평가 : "); double 수행평가 = scanner.nextDouble();
+		중간고사 = 중간고사 * 0.3; 기말고사 *=0.3; 수행평가 *=0.4;
+		System.out.printf(" 반영비율 총점 :%.f \n" , (중간고사+기말고사+수행평가)
+		//문제14
+		//
+		
+		
+		
+		
+		
+		
+	}
+
+}
